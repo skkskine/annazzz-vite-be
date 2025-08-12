94a82e35a077be8b7ce161515fbd2df22d110530
Gianmaria Schinezos <gnmr@Gianmarias-Laptop.local>
2025-08-12 17:15:56 +0200

    add vercel.jsond

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -156,7 +156,6 @@ export interface User {
 export interface Media {
   id: number;
   alt: string;
-  prefix?: string | null;
   updatedAt: string;
   createdAt: string;
   url?: string | null;
@@ -306,7 +305,6 @@ export interface UsersSelect<T extends boolean = true> {
  */
 export interface MediaSelect<T extends boolean = true> {
   alt?: T;
-  prefix?: T;
   updatedAt?: T;
   createdAt?: T;
   url?: T;
